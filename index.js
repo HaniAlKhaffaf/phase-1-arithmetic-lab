ac95333aaf6f0d1174739a873d3d83675d89390d
Hani Amer <cvba2222@gmail.com>
2022-12-10 06:17:10 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+const num1 = 2;
+const num2 = 31
+
+let multiply = num1 * num2;
+
+let random = Math.round(Math.random()+1);
+
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4;
+
+const max = Math.max(1, 2, 3, 20);
